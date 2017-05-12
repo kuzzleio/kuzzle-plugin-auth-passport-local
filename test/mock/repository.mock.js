454a0460e6f0893e7b161d07fff3d65a92f65ee3
David Bengsch <dbengsch@kaliop.com>
2017-05-12 16:31:20 +0200

    Secret is not useful anymore as each credential record holds his own salt

--- test/mock/repository.mock.js
+++ test/mock/repository.mock.js
@@ -2,7 +2,8 @@ module.exports = function () {
   return {
     get: kuid => kuid === 'ghost' ? Promise.resolve(null) : Promise.resolve({
       _id: 'foo',
-      userPassword: 'dc7fcdf5a547d21d517c14e3a51b8f51e7a6bdba7b1eedcc44605fc0c450b710',
+      userPassword: '7ea233545318d00c0bc668cbcbf5d7f25416ebc8afe89717c07d4ad48badd1a162a8194075385047d8b548b8903ac60a8c131836731e647911a52fc0233a698ba3f79070e627555de3d80bff88dbb6d9ed0a2dcab2d229c2b0622e4f42a93e573aa1346d292bf30f7a0bdc41f46dcfea209b939582dddbe1ccf37ea499937fbb984ce4054a821a5ae4f43a118a8a9989c66df5787a0d96626e7ec5cea51a1998542663ff19fb4f0cd7414e01be0cdbf00bec37acc0657e009668b8ad01c34a46ac4c1f9bc8865bc3944083010623e2ba4da1535e90f25357a236d1e207bce6a3c8b9e9245ff265bd712a830bd6cbde763fcd6d4f5a0833d84098c0445e517020',
+      userSalt: 'someSalt',
       kuid
     }),
     search: () => Promise.resolve({total: 1, hits: [{_id: 'foo2', kuid: 'someId'}]}),
