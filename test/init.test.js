ff005dfb1179e5362dee0e2859298bde6d192021
Sébastien Cottinet <seb.cottinet@gmail.com>
2018-07-09 14:13:14 +0200

    change strategy definition depending on kuzzle version (#56)

--- test/init.test.js
+++ test/init.test.js
@@ -1,18 +1,50 @@
 const
   should = require('should'),
-  PluginLocal = require('../lib');
+  PluginLocal = require('../lib'),
+  PluginContext = require('./mock/pluginContext.mock.js');
 
 describe('#init', () => {
-  const pluginContext = require('./mock/pluginContext.mock.js');
-  let pluginLocal;
+  let
+    pluginLocal,
+    pluginContext;
 
   beforeEach(() => {
+    pluginContext = new PluginContext();
     pluginLocal = new PluginLocal();
     pluginLocal.context = pluginContext;
   });
 
   it('should initialize a \'local\' strategy', () => {
     should(pluginLocal.strategies).not.be.ok();
+
+    pluginLocal.init(null, pluginContext);
+
+    should(pluginLocal.strategies).be.Object().and.match({
+      local: {
+        config: {
+          authenticator: 'LocalStrategy',
+          strategyOptions: {},
+          authenticateOptions: {
+            scope: []
+          },
+          fields: ['username', 'password']
+        },
+        methods: {
+          create: 'create',
+          delete: 'delete',
+          exists: 'exists',
+          getInfo: 'getInfo',
+          update: 'update',
+          validate: 'validate',
+          verify: 'verify'
+        }
+      }
+    });
+  });
+
+  it('should use a "constructor" instead if Kuzzle version is < 1.4.0', () => {
+    should(pluginLocal.strategies).not.be.ok();
+    pluginContext.config.version = '1.3.9999';
 
     pluginLocal.init(null, pluginContext);
 
