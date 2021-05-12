e9978b37b11712fa56e7c51b084b583fc7ff0f4d
Mathieu Veber <math.veber@gmail.com>
2021-05-12 17:32:30 +0200

    Add tests

---
+++ test/utils/objectRecursiveMap.test.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const should = require('should');
+const sinon = require('sinon');
+const { objectRecursiveMap } = require('../../lib/utils');
+
+describe('#search', () => {
+  let object;
+  let fn;
+
+  beforeEach(() => {
+    object = {
+      foo: 'foo',
+      bar: { a: 'bar.a', b: 'bar.b' },
+      c: [{ array: 'c'}, { array: 'c' }]
+    };
+    fn = sinon.stub().returns({});
+  });
+
+  it('should apply a function on each property and return the object', () => {
+    const result = objectRecursiveMap(object, fn);
+
+    should(fn).be.called(7);
+    should(result).be.deepEqual(object);
+  });
+
+  it('should handle new key or value for mapped objects', () => {
+    const result = objectRecursiveMap(object, (key) => {
+      if (key === 'array') {
+        return { key: 'parent'};
+      }
+      else if (key === 'b') {
+        return { key: 'c', value: 'nested' };
+      }
+      return { value: 'default' };
+    });
+
+    should(result).be.deepEqual({
+      foo: 'default',
+      bar: { a: 'default', c: 'nested' },
+      c: [{ parent: 'c' }, { parent: 'c' }]
+    });
+  });
+});
