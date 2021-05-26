b7369394258d20a298b7dfd87cf4d99decb87c3e
Mathieu Veber <43341681+MathieuVeber@users.noreply.github.com>
2021-05-26 14:57:02 +0200

    Implement search method (#76)
    ## What does this PR do?
    Adds a method that can retrieve users from an ES query about their username. This method has to watch both input and output. On one hand the user should be able to search __ONLY__ with username and on the other hand the user results are sensitive informations so this method will only return kuid.
    
    ### Additional context
    Check this [PR](https://github.com/kuzzleio/kuzzle/pull/1948) which has an example, some documentation and more concerning this feature and the linked Kuzzle main action.
    
    ### Additional safety considerations
    
    This search method require more security than usual and needs at the same time a whitelist and a blacklist:
    
    - If a query is given, it must include `username` and exlude both ES `multi_match` keyword and other mapping properties
    - If a sort is given, it must include `username` or `kuid` and exclude other mapping properties
    
    ES keywords are a pain point because we are dependent to what ES decide in the future so we need to keep an eye on them and update the blacklist if necessary. To decrease the security concerns, I have set up a second [safety feature](https://github.com/kuzzleio/kuzzle-plugin-auth-passport-local/pull/76/files#diff-447ddb2a739978f651b1db98aaafd221202e10030a81de2c1f309fa2b1ba5ed1R85) which has its limits but does better than nothing in case a new ES keyword comes out of nowhere.

---
+++ test/search.test.js
@@ -0,0 +1,128 @@
+const should = require('should');
+const PluginLocal = require('../lib');
+const PluginContext = require('./mock/pluginContext.mock.js');
+
+describe('#search', () => {
+  const pluginContext = new PluginContext();
+  let pluginLocal;
+  let searchBody;
+
+  beforeEach(async () => {
+    pluginLocal = new PluginLocal();
+    await pluginLocal.init({}, pluginContext);
+    pluginLocal.userRepository = new (require('./mock/getUserRepository.mock')(pluginLocal))();
+
+    searchBody = {
+      query: {
+        match: {
+          username:  'foo2'
+        }
+      },
+      sort: [
+        { kuid: 'asc' },
+        { username: 'desc' }
+      ]
+    };
+  });
+
+  it('should return a search result', async () => {
+    const result = await pluginLocal.search(searchBody);
+
+    should(result).eql({ total: 1, hits: [{ kuid:'someId', username: 'foo2' }] });
+  });
+
+  it('should throw an error if the query contains forbidden fields', () => {
+    searchBody.query.match = { algorithm: 'sha512' };
+
+    return should(pluginLocal.search(searchBody))
+      .be.rejectedWith(new pluginLocal.context.errors.ForbiddenError('Forbidden field "algorithm". Only the "username" or "kuid" fields are sortable and only the first is also searchable.'));
+  });
+
+  it('should throw an error if the query contains forbidden keyword', () => {
+    searchBody.query = {
+      multi_match: {
+        query: 'sha512', 
+        fields: [ 'username', 'algorithm' ] 
+      }
+    };
+
+    return should(pluginLocal.search(searchBody))
+      .be.rejectedWith(new pluginLocal.context.errors.BadRequestError('The "multi_match" keyword is not allowed in this search query for security concerns.'));
+  });
+
+  it('should throw an error if the query does not concern username', () => {
+    searchBody.query = {
+      evil_keyword: {
+        query: 'password', 
+        fields: [ 'user.*', 'undetectable_wilcard' ] 
+      }
+    };
+
+    return should(pluginLocal.search(searchBody))
+      .be.rejectedWith(new pluginLocal.context.errors.ForbiddenError('Only the "username" field is searchable, otherwise leave the query empty.'));
+  });
+
+  it('should throw an error if the sort contains forbidden fields', () => {
+    searchBody.sort = [{ 'passwordHistory.userSalt': 'asc' }];
+
+    return should(pluginLocal.search(searchBody))
+      .be.rejectedWith(new pluginLocal.context.errors.ForbiddenError('Forbidden field "passwordHistory.userSalt". Only the "username" or "kuid" fields are sortable and only the first is also searchable.'));
+  });
+
+  it('should throw an error if the sort does not concern username or kuid', () => {
+    searchBody.sort = [{
+      evil_keyword: {
+        order : 'asc',
+        fields: ['user.*', 'undetectable_wilcard']
+      }
+    }];
+
+    return should(pluginLocal.search(searchBody))
+      .be.rejectedWith(new pluginLocal.context.errors.ForbiddenError('Only the "username" or "kuid" fields are sortable.'));
+  });
+
+  it('should not detect required fields when they are part of an array values', async () => {
+    // In ES multi fields queries, we can use wildcards which are almost undetectable.
+    // If username is in an array, it is likely that multiple fields are listed in there...
+    searchBody.query = {
+      new_unknown_keyword: {
+        query: 'password', 
+        fields: [ 'username', 'undetectable_wilcard', 'user.*' ] 
+      }
+    };
+
+    await pluginLocal.search(searchBody)
+      .should.be.rejectedWith(new pluginLocal.context.errors.ForbiddenError('Only the "username" field is searchable, otherwise leave the query empty.'));
+    
+    // But we cannot just throw if an array has non compliant values, this should actually work:
+    searchBody.query = {
+      terms : {
+        username: ['username', 'algorithm', 'foo2']
+      }
+    };
+
+    const result = await pluginLocal.search(searchBody);
+
+    should(result).eql({ total: 1, hits: [{ kuid:'someId', username: 'foo2' }] });
+
+    // Currently, there is just half the solution. A complex query can still slip through the net.
+    // Conclusion: Keep up to date forbiddenKeywords to prevent queries on forbiddenFields
+
+    // Complex query example:
+    // searchBody.query = {
+    //   bool: {
+    //     must: {
+    //       match: {
+    //         username: 'foo2'
+    //       }
+    //     },
+    //     filter: {
+    //       new_unknown_keyword: {
+    //         query: 'password', 
+    //         fields: [ 'user.*', 'undetectable_wilcard' ] 
+    //       }
+    //     }
+    //   }
+    // };
+  });
+});
